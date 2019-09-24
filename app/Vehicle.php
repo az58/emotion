91225f1d5e3cc0b1f518003af45362913b46f229
Lory Leticee <l.leticee@skiset.com>
2019-09-24 11:13:11 +0200

    test ident

--- app/Vehicle.php
+++ app/Vehicle.php
@@ -6,10 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Vehicle extends Model
 {
-    protected  $connection = 'mysql';
-    protected $table = 'vehicle';
-    protected $primaryKey = 'id';
-    public $timestamps = true;
+    protected  $connection 	= 'mysql';
+    protected $table 		= 'vehicle';
+    protected $primaryKey 	= 'id';
+    public $timestamps 		= true;
 
     public function booking()
     {
