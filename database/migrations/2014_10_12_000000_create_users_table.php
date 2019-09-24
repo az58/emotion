2c033fe860ce615ae964dda2bedf8506d693d9ca
Lory Leticee <l.leticee@skiset.com>
2019-09-24 14:23:33 +0200

    Faker fixture

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('role',['admin', 'anonym', 'buyer']);
+            $table->enum('role',['admin', 'anonym', 'buyer'])->default('buyer');
             $table->string('ip_address', 255)->nullable();
             $table->rememberToken();
             $table->timestamps();
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,10 @@
 <?php
 
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+use Faker\Factory as Faker;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +15,45 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+		$faker = Faker::create();
+		foreach (range(1,20) as $index) {
+			DB::table('users')->insert([
+				'firstname' => $faker->firstName,
+				'lastname' 	=> $faker->lastName,
+				'email'		=> $faker->email,
+				'password' 	=> bcrypt('secret'),
+			]);
+		}
+
+		DB::table('vehicle')->insert([
+			'category' 		=> $faker->randomElement(['car', 'scooter']),
+			'brand' 		=> $faker->lastName,
+			'type'			=> $faker->randomAscii,
+			'color' 		=> $faker->colorName,
+			'licence_plate' => $faker->randomAscii,
+			'distance' 		=> $faker->numberBetween(5,800),
+			'date_purchase' => $faker->date('Y-m-d','now'),
+			'buying_price' 	=> $faker->randomNumber(4),
+			'fuel_level' 	=> $faker->randomNumber(2),
+			'fuel_brand' 	=> $faker->randomElement(['diesel', 'essence98', 'essence95']),
+		]);
+
+		foreach (range(1,20) as $index) {
+			DB::table('booking')->insert([
+
+				'user_id' 			=> $faker->numberBetween(1,20),
+				'vehicle_id' 		=> 1,
+				'start_date'		=> $faker->date('Y-m-d','now'),
+				'end_date' 			=> $faker->date('Y-m-d','now'),
+				'state' 			=> $faker->city,
+				'booking_price' 	=> $faker->numberBetween(1,100),
+				'age' 				=> $faker->numberBetween(18,65),
+				'address' 			=> $faker->randomNumber(3).' rue '.$faker->name.' '.$faker->randomNumber(5).' '.$faker->city,
+				'driving_licence' 	=> $faker->randomNumber(5),
+				'cb_number' 		=> $faker->creditCardNumber,
+				'cb_expire' 		=> $faker->randomNumber(4),
+				'cb_cvv' 			=> $faker->randomNumber(3),
+			]);
+		}
     }
 }
