800d1f573b5289a386364cde9bad437859cd9585
Lory Leticee <l.leticee@skiset.com>
2019-10-15 09:17:51 +0200

    add constant in tools an a declar strict type

--- app/Http/Controllers/Vehicle/VehicleController.php
+++ app/Http/Controllers/Vehicle/VehicleController.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers\Vehicle;
 
 use App\Http\Controllers\Controller;
@@ -11,26 +13,23 @@ use Illuminate\Http\Request;
 
 class VehicleController extends Controller
 {
-	protected $_iDays						= 1;
-	protected $_starHour					= '07:00';
-	protected $_endHour						= '18:00';
-	protected $_sPlace						= 'paris';
+
 
 	/**
 	 * Récupère les elements de recherche d'un vehicule entrées par lutilisateur et retourne une liste correspondante
 	 * @param Request $request
 	 * @return \Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|\Illuminate\View\View
 	 */
-	public function search(Request $request){
-        if (!$request->has(Constant::$needles)) {
+	public function search(Request $request) {
+        if (!$request->has(Constant::NEEDLES)) {
             $vehicles 						= Vehicle::all();
 
-            $iDays                          = $this->_iDays;
+            $iDays                          = Constant::DAYS;
             $startDate                      = Date('m/d/Y', time());
             $endDate                        = Date('m/d/Y', time());
-            $starHour                      	= $this->_starHour;
-            $endHour                        = $this->_endHour;
-            $sPlace                         = $this->_sPlace;
+            $starHour                      	= Constant::STARTHOUR;
+            $endHour                        = Constant::ENDHOUR;
+            $sPlace                         = Constant::PLACE;
 
             return view('vehicle/show', compact('vehicles' , 'iDays', 'startDate', 'endDate', 'sPlace', 'starHour', 'endHour'));
         }
@@ -48,7 +47,7 @@ class VehicleController extends Controller
 
         $iMaxPrice                          = (is_numeric($request->price_end) ? $request->price_end : null) ;
 
-        $sCat                               = in_array(strtolower($sCategory),Constant::$categories) ;
+        $sCat                               = in_array(strtolower($sCategory),Constant::CATEGORIES) ;
         if (!$sCat){
             $vehicles 						= Vehicle::where('current_place', htmlentities($sPlace))->whereBetween('day_price', [0, $iMaxPrice])->get();
         }
@@ -57,7 +56,7 @@ class VehicleController extends Controller
             $vehicles 						= Vehicle::where('current_place', htmlentities($sPlace))->where('category', $sCategory)->whereBetween('day_price', [0, $iMaxPrice])->get();
         }
 
-        if (!$vehicles->isEmpty()){
+        if (!$vehicles->isEmpty()) {
             if (!Tools::validateDate($startDate) || !Tools::validateDate($endDate)) {
                redirect('vehicle/search');
             }
